7489cad7c508cc7b5af25cbf3a0450375d755867
Adit Hattikudur <hattikud@usc.edu>
2023-03-29 11:30:36 -0700

    Progress 3/29

--- site/src/App.js
+++ site/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Other from "./pages/Other";
+import MyWatchlists from "./pages/MyWatchlists";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         {/* Root pages, located in /pages/ */}
         <Route path="/" element={<Home />} />
         <Route path="/other" element={<Other />} />
+        <Route path="/MyWatchlists" element={<MyWatchlists />} />
         {/* 404 page not found redirect */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
