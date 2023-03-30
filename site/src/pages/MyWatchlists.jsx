157a70cea45e5273533a158b5eaaff91e97e7866
shukl12898 <shuklaak@usc.edu>
2023-03-29 22:27:40 -0700

    Warnings fix

--- site/src/pages/MyWatchlists.jsx
+++ site/src/pages/MyWatchlists.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import OneWatchlist from "../components/OneWatchlist";
 import MultipleWatchlists from "../components/MultipleWatchlists";
