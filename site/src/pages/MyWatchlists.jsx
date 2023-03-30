1f9a123531a35cc8af35b15d04421a2dd482c3ae
Adit Hattikudur <hattikud@usc.edu>
2023-03-29 17:47:27 -0700

    Progress 3/29 5:47pm

--- site/src/pages/MyWatchlists.jsx
+++ site/src/pages/MyWatchlists.jsx
@@ -4,6 +4,68 @@ import OneWatchlist from "../components/OneWatchlist";
 import MultipleWatchlists from "../components/MultipleWatchlists";
 
 // This page provides a button with a redirect to "/other"
+
+const moviesList = [{
+  id: 0, // Used in JSX as a key
+  title: 'Flight',
+  des: 'mathematician',
+}, {
+  id: 1, // Used in JSX as a key
+  title: 'Train',
+  des: 'chemist',
+}, {
+  id: 2, // Used in JSX as a key
+  title: 'Plane',
+  des: 'physicist',
+}, {
+  id: 3, // Used in JSX as a key
+  title: 'Boat',
+  des: 'chemist',
+}];
+
+const multiList = [
+{
+  wName: "watchlist1",
+  list: [{
+    id: 0, // Used in JSX as a key
+    title: 'Flight',
+    des: 'mathematician',
+  }, {
+    id: 1, // Used in JSX as a key
+    title: 'Train',
+    des: 'chemist',
+  }, {
+    id: 2, // Used in JSX as a key
+    title: 'Plane',
+    des: 'physicist',
+  }, {
+    id: 3, // Used in JSX as a key
+    title: 'Boat',
+    des: 'chemist',
+  }]
+ },
+{
+  wName: "watchlist2",
+  list: [{
+      id: 4, // Used in JSX as a key
+      title: 'Owl',
+      des: 'mathematician',
+    }, {
+      id: 5, // Used in JSX as a key
+      title: 'Bird',
+      des: 'chemist',
+    }, {
+      id: 6, // Used in JSX as a key
+      title: 'Eagle',
+      des: 'physicist',
+    }, {
+      id: 7, // Used in JSX as a key
+      title: 'Parrot',
+      des: 'chemist',
+    }]
+}
+];
+
 function MyWatchlists() {
   // fetchResponse is a constant in this component's state. Use handleFetchResponse(newValue)
   // to update the value of fetchResponse
@@ -24,8 +86,8 @@ function MyWatchlists() {
         Click to go to Home page
       </button>
       <div>
-        <OneWatchlist/>
-        <MultipleWatchlists/>
+        {/* }<OneWatchlist movies = {moviesList} /> */}
+        <MultipleWatchlists watchlistsArr = {multiList} />
       </div>
     </div>
   );
