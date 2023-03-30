899f9bce144bb4bc4d3d9aca96f739b8de216d37
hiromiki <hmiki@usc.edu>
2023-03-30 16:50:36 -0700

    issue 14 fix

--- site/src/App.test.js
+++ site/src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import {screen, waitFor } from "@testing-library/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
