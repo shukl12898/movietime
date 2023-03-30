365aed641bf056cce118041e598654e7528d9068
shukl12898 <shuklaak@usc.edu>
2023-03-29 22:43:44 -0700

    Centralized home page

--- site/src/pages/Home.jsx
+++ site/src/pages/Home.jsx
@@ -1,70 +1,43 @@
-import React, { useState, useEffect } from "react";
-import MovieResult from '../components/MovieResult';
-import SearchBox from '../components/SearchBox';
-import SearchFilter from '../components/SearchFilter';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 // This page provides a button with a redirect to "/other"
 function Home() {
-  //empty array for movie results
-  const [movies, assignMovies] = useState([]);
-  const [query, setQuery]= useState('');
-  const [resultCount, setResultCount] = useState(10);
-  const [selectedFilter, setSelectedFilter]= useState("movie");
+  // fetchResponse is a constant in this component's state. Use handleFetchResponse(newValue)
+  // to update the value of fetchResponse
+  const [fetchResponse, handleFetchResponse] = useState();
 
-  const options=[
-      {label: "Movie Title", value: "movie"},
-      {label: "Keyword", value: "keyword"},
-      {label: "Actor/Actress", value: "person"}];
-  const handleSelectFilter = (selectedOption) => {
-      setSelectedFilter(selectedOption);
-  };
-
-
-  const getMoreResults = () => {
-      setResultCount((resultCount+10));
-  }
-
-  const getSearchResults = async(query,selectedFilter) => {
-
-      const API_URL= 'https://api.themoviedb.org/3/search/' + selectedFilter + '?api_key=f0a2d3c27e0522ee834ad2e76ceeebb1&query='+ query;
-
-      try{
-          const response = await fetch(API_URL);
-
-          const responseJson = await response.json();
-
-          console.log(responseJson.results);
-
-          if (responseJson.results) {
-              assignMovies(responseJson.results)
-          }
-      }
-      catch (e) {
-          console.log(e);
-      }
-
-
-  };
-
-  useEffect(()=>{
-      getSearchResults(query,selectedFilter)
-  },[query,selectedFilter]);
+  // Calling navigate() will allow us to redirect the webpage
+  const navigate = useNavigate();
 
   // Anything returned will be rendered in React
   return (
     <div>
-        <h1>Search</h1>
-        <div className = "row">
-            <div className="col">
-                <SearchFilter options = {options} onSelect={handleSelectFilter}/>
-            </div>
-            <div className = "col">
-                <SearchBox query ={query} setQuery = {setQuery}/>
-            </div>
+      <div>Home Page</div>
+      <button
+        onClick={() => {
+          navigate("/other");
+        }}
+      >
+        Click to go to Other page
+      </button>
+      <button
+              onClick={() => {
+                navigate("/Login");
+              }}
+            >
+              Click to go to Login page
+       </button>
+       <button
+           onClick={() => {
+             navigate("/Search");
+           }}>
+              Click to go to Search page
+       </button>
+      {/* Conditionally render this div if fetchResponse is a valid value */}
+      {fetchResponse ? <div>{fetchResponse}</div> : null}
 
-        </div>
-        <MovieResult movies = {movies} filter = {selectedFilter} numResults = {resultCount}/>
-        <button onClick={getMoreResults}>Load More</button>
     </div>
   );
 }
