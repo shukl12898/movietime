496e6c233e94494b4105e016dc4baba2df0c6c58
shukl12898 <shuklaak@usc.edu>
2023-03-29 22:46:09 -0700

    Centralized home page warnings fix

--- site/src/pages/Home.jsx
+++ site/src/pages/Home.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 
 // This page provides a button with a redirect to "/other"
 function Home() {
-  // fetchResponse is a constant in this component's state. Use handleFetchResponse(newValue)
-  // to update the value of fetchResponse
-  const [fetchResponse, handleFetchResponse] = useState();
-
   // Calling navigate() will allow us to redirect the webpage
   const navigate = useNavigate();
 
@@ -35,8 +31,6 @@ function Home() {
            }}>
               Click to go to Search page
        </button>
-      {/* Conditionally render this div if fetchResponse is a valid value */}
-      {fetchResponse ? <div>{fetchResponse}</div> : null}
 
     </div>
   );
