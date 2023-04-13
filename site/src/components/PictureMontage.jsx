a697c087e2205f834c2c3f29f7e54677a5b7731e
shukl12898 <shuklaak@usc.edu>
2023-04-13 16:17:22 -0700

    Picture montage basic UI fixes

--- site/src/components/PictureMontage.jsx
+++ site/src/components/PictureMontage.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import '../styles/picture-montage.css';
 
-const PictureMontage = ({ movieImages }) => {
-  // Anything returned will be rendered in React
+const PictureMontage = ({ movieImages, className }) => {
   return (
-    <div className="montage">
+    <div className={`montage ${className}`}>
       {movieImages.map((image, index) => (
-        <img key={index} src={image} alt={`Image ${index}`} />
+        <img
+          key={index}
+          src={image}
+          alt={`Image ${index}`}
+          style={{
+            top: `calc(-50% + (${20 * (Math.random() - 0.5) * index}px))`,
+            left: `calc(-50% + (${20 * (Math.random() - 0.5) * index}px))`,
+            transform: `rotate(${Math.floor(Math.random() * 91) - 45}deg)`,
+            position: 'absolute',
+          }}
+        />
       ))}
     </div>
   );
-}
+};
 
 export default PictureMontage;
