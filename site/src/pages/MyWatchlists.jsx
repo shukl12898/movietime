5ed11cd706c0cc9a89626c3c6b609bf8cdf82e12
Oscar Clavijo <oclavijo@usc.edu>
2023-04-18 23:26:48 -0700

    Update MyWatchlists.jsx

--- site/src/pages/MyWatchlists.jsx
+++ site/src/pages/MyWatchlists.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MultipleWatchlists from "../components/MultipleWatchlists";
-import WatchlistOptions from "../components/WatchlistOptions";
 import NavBar from "../components/NavBar";
 
 function MyWatchlists() {
