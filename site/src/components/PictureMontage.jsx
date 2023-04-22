e8bc07e05835524cc4c1cca522de47083b573b0d
shukl12898 <shuklaak@usc.edu>
2023-04-21 19:30:58 -0700

    10 images testing

--- site/src/components/PictureMontage.jsx
+++ site/src/components/PictureMontage.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const PictureMontage = ({ movieImages }) => {
   return (
-    <div className="montage">
+    <div className="montage" style={{ position: 'relative', width: '500px', height: '400px' }}>
       {movieImages.map((image, index) => (
         <img
           key={index}
           src={image}
           alt={`Image ${index}`}
           style={{
-            top: `calc(-50% + (${20 * (Math.random() - 0.5) * index}px))`,
-            left: `calc(-50% + (${20 * (Math.random() - 0.5) * index}px))`,
+            top: `${20 * (Math.random() - 0.5) * index}px`,
+            left: `${20 * (Math.random() - 0.5) * index}px`,
             transform: `rotate(${Math.floor(Math.random() * 91) - 45}deg)`,
             position: 'absolute',
-            width: '500px',
-            height: '400px',
+            width: '100%',
+            height: '100%',
           }}
         />
       ))}
