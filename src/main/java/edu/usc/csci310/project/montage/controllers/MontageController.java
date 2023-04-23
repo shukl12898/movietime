4a6da7b6b4b9b136c0afb6bc615b1495d29fcfc1
shukl12898 <shuklaak@usc.edu>
2023-04-22 18:10:32 -0700

    Picture montage backend, front end and tests working

---
+++ src/main/java/edu/usc/csci310/project/montage/controllers/MontageController.java
@@ -0,0 +1,30 @@
+package edu.usc.csci310.project.montage.controllers;
+import edu.usc.csci310.project.montage.responses.MontageResponse;
+import edu.usc.csci310.project.montage.service.MontageService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class MontageController {
+    private final MontageService service;
+
+    public MontageController(MontageService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/images/{ids}")
+    public ResponseEntity<?> getMovieImages(@PathVariable ArrayList<Integer> ids){
+        MontageResponse response = service.getImages(ids);
+        try{
+            return new ResponseEntity<>(response, HttpStatus.OK);
+
+        } catch (Exception ex){
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
