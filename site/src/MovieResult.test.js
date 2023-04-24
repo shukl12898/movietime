ea569d368876391d2278f0510178a17e03014567
shukl12898 <shuklaak@usc.edu>
2023-04-23 20:47:36 -0700

    100% jest coverage on movie result passing

--- site/src/MovieResult.test.js
+++ site/src/MovieResult.test.js
@@ -1,31 +1,38 @@
 import React from 'react';
-import { render, screen, act } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import MovieResult from './components/MovieResult';
-import MovieDetails from './components/MovieDetails';
 
-describe('MovieResult component', () => {
-  it('should render a MovieDetails component for each movie in the props', async () => {
-    const mockDataMovie = {
-      id: 1,
-      original_title: 'The Matrix',
-    };
-    const props = {
-      movies: [mockDataMovie],
-      numResults: 1,
-      filter: 'movie'
-    };
+jest.mock('./components/MovieDetails', () => {
+  return function DummyMovieDetails(props) {
+    return (
+      <div data-testid="movie-details">
+        <span>{props.data.title}</span>
+        <span>{props.filter}</span>
+      </div>
+    );
+  };
+});
 
-    jest.spyOn(global, 'fetch').mockResolvedValue({
-      ok: true,
-      json: () => Promise.resolve(mockDataMovie)
-    });
+describe('MovieResult', () => {
+  it('renders movie titles and filters for the first n movies in the list', async () => {
+    const movies = [
+      { id: 1, title: 'Movie 1' },
+      { id: 2, title: 'Movie 2' },
+    ];
+    const numResults = 1;
+    const filter = 'Action';
 
-    await act(async () => {
-      await render(<MovieResult {...props} />);
-    });
+    const { getByTestId, queryByText } = render(
+      <MovieResult movies={movies} numResults={numResults} filter={filter} />
+    );
 
-    expect(screen.queryByText('The Matrix')).toBeInTheDocument();
-    const movieDetailsList = screen.getAllByTestId('movie-title');
-    expect(movieDetailsList).toHaveLength(1);
+    // Ensure that the first two movie titles are displayed
+    expect(getByTestId('movie-details')).toHaveTextContent('Movie 1');
+
+    // Ensure that the third movie title is not displayed
+    expect(queryByText('Movie 2')).not.toBeInTheDocument();
+
+    // Ensure that the filter prop is passed to the MovieDetails component
+    expect(getByTestId('movie-details')).toHaveTextContent('Action');
   });
 });
