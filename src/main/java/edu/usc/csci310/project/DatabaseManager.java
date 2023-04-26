ae70aca96ecd3f31dacebac6df45c19f324442f3
Adit Hattikudur <hattikud@usc.edu>
2023-04-25 18:09:54 -0700

    4/25 Test Progression 6:09

--- src/main/java/edu/usc/csci310/project/DatabaseManager.java
+++ src/main/java/edu/usc/csci310/project/DatabaseManager.java
@@ -15,6 +15,7 @@ public class DatabaseManager {
     private static final String USERS_TABLE = "CREATE TABLE IF NOT EXISTS " +
             "users (user_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
             "username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL)";
+    private static Connection c;
 
     private static final String WATCHLIST_USER_TABLE = "CREATE TABLE IF NOT EXISTS " +
             "watchlists (watchlist_id INTEGER PRIMARY KEY AUTOINCREMENT, list_name VARCHAR(255) NOT NULL, " +
@@ -116,7 +117,7 @@ public class DatabaseManager {
      */
     public List<String> getAllUsers(int limit) {
         List<String> userNames = new ArrayList<>();
-        try (Connection c = DriverManager.getConnection(SQLITE_CONNECTION_STRING)){
+        try (Connection c = DriverManager.getConnection(SQLITE_CONNECTION_STRING)) {
             PreparedStatement pst = c.prepareStatement("SELECT * from users LIMIT ?");
             pst.setInt(1, limit);
             ResultSet resultSet = pst.executeQuery();
