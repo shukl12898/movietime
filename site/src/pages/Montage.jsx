eb893f341e2897d9a7408bb64e7c66b198f707ef
shukl12898 <shuklaak@usc.edu>
2023-04-26 08:53:26 -0700

    Build fail fix

--- site/src/pages/Montage.jsx
+++ site/src/pages/Montage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import PictureGrid from '../components/PictureGrid';
 import NavBar from '../components/NavBar';
