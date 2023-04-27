3357e0fb2eb80dcb171656b2a21ac2facb1b6ba1
shukl12898 <shuklaak@usc.edu>
2023-04-27 03:10:11 -0700

    Movie details made inline

--- site/src/components/MovieDetails.jsx
+++ site/src/components/MovieDetails.jsx
@@ -1,142 +1,115 @@
-import React from "react";
-import {useState, useEffect} from "react";
-import '../styles/movie-details.css';
-import HoverButtons from '../components/HoverButtons';
-
+import React, { useState, useEffect } from "react";
+import "../styles/movie-details.css";
+import HoverButtons from "../components/HoverButtons";
 import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  Image,
+  Box,
   Badge,
-   Accordion,
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    AccordionIcon,
-    Box,
-} from '@chakra-ui/react';
+  Image,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react";
 
 function MovieDetails(props) {
-
   const [movieDetails, setMovieDetails] = useState(null);
   const [isHovering, setIsHovering] = useState(false);
-  const [showOverlay, setShowOverlay] = useState(false);
   const [selectedMovieID, setSelectedMovieID] = useState(null);
+  const [castIsOpen, setCastIsOpen] = useState(false);
 
   const movieID = props.data;
 
-    useEffect(() => {
-        fetch(`/movies/${movieID}`)
-            .then(response => {
-                if(response.status === 400) {
-                    throw new Error(response.json());
-                }
-                return response.json();
-            })
-            .then(data => setMovieDetails(data))
-            .catch(error => console.error(error));
-    }, [movieID]);
+  useEffect(() => {
+    fetch(`/movies/${movieID}`)
+      .then((response) => {
+        if (response.status === 400) {
+          throw new Error(response.json());
+        }
+        return response.json();
+      })
+      .then((data) => setMovieDetails(data))
+      .catch((error) => console.error(error));
+  }, [movieID]);
 
-    const showDetails = (movieID) => {
-        setShowOverlay(true);
-        setSelectedMovieID(movieID);
+  const showDetailsToggle = (movieID) => {
+    if (selectedMovieID === movieID) {
+       setSelectedMovieID(null);
+    } else {
+       setSelectedMovieID(movieID);
     }
+  };
 
-    const handleMouseEnter = () => {
-        setIsHovering(true);
-      };
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
 
-      const handleMouseLeave = () => {
-        setIsHovering(false);
-      };
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
 
-    if (!movieDetails) {
-            return <div>Loading...</div>;
-    }
+  if (!movieDetails) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-
-        <div className="background">
-            {movieDetails.title ?
-              <Box
-                p={3}
-                id="movie-name"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                key={movieID}
-              >
-                <div
-                  className="movie-title"
-                  data-testid="movie-title"
-                  onClick={() => {
-                    showDetails(movieID);
-                  }}
-                >
-                  {movieDetails.title}
-                </div>
-                {isHovering && <HoverButtons movieDetails={movieDetails} className="hover" />}
+  return (
+    <Box className="background">
+      {movieDetails.title && (
+        <Box
+          p={3}
+          id="movie-name"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          key={movieID}
+          position="relative"
+          zIndex="1"
+        >
+          <div
+            className="movie-title"
+            data-testid="movie-title"
+            onClick={() => {
+              showDetailsToggle(movieID);
+            }}
+          >
+            {movieDetails.title}
+          </div>
+          {isHovering && <HoverButtons movieDetails={movieDetails} className="hover" />}
+          {selectedMovieID && (
+            <Box id="overlay-content" data-testid="overlay" mt={2} padding="20px" borderWidth="1px" borderRadius="md" boxShadow="lg" width="400px">
+              <Box mb={2}>
+                <Badge>Released {movieDetails.year}</Badge>
               </Box>
-              :
-              <div>Loading...</div>
-            }
-
-        {showOverlay && selectedMovieID && (
-
-        <Modal isOpen={showOverlay} onClose={setShowOverlay} >
-            <ModalOverlay />
-                <ModalContent data-testid="overlay" id="overlay-content">
-                  <ModalHeader>
-                    {movieDetails.title}
-                    <br />
-                    <Badge>Released {movieDetails.year} </Badge >
-                  </ModalHeader>
-                  <ModalCloseButton data-testid="closeButton"/>
-                  <ModalBody>
-                    <br />
-                      <Image src={movieDetails.poster} />
-                    <br />
-                        {movieDetails.overview}
-                    <br />
-                      Genres: {movieDetails.genres.map((genre) => genre).join(", ")}
-
-                    <br />
-                    <br />
-                    <Accordion>
-                       <AccordionItem>
-                         <h2>
-                           <AccordionButton>
-
-                               Cast List
-
-                             <AccordionIcon />
-                           </AccordionButton>
-                         </h2>
-                         <AccordionPanel maxH="200px" overflowY="scroll" id="scrollContainer">
-                            {movieDetails.cast.map((member,index) =>
-                              <li key={index} data-testid="cast">{member}</li>
-                            )}
-                         </AccordionPanel>
-                       </AccordionItem>
-                    </Accordion>
-                    <br />
-                    {movieDetails.director}
-                    <br/>
-                    {movieDetails.productionCompanies.map((company) => company).join(", ")}
-                    <br/>
-                      </ModalBody>
-                      <ModalFooter>
-                      </ModalFooter>
-                    </ModalContent>
-             </Modal>
-
-
-            )}
-            </div>
-    );
+              <Box d="flex">
+                <Image src={movieDetails.poster} mr={2} />
+                <Box>
+                  <Box mb={2}>{movieDetails.overview}</Box>
+                  <Box mb={2}>
+                    Genres: {movieDetails.genres.map((genre) => genre).join(", ")}
+                  </Box>
+                  <Accordion mb={2} allowToggle>
+                    <AccordionItem>
+                      <h2>
+                        <AccordionButton onClick={() => setCastIsOpen(!castIsOpen)}>Cast List</AccordionButton>
+                      </h2>
+                      <AccordionPanel maxH="200px" overflowY="scroll" isOpen={castIsOpen}>
+                        {movieDetails.cast.map((member, index) => (
+                          <li key={index} data-testid="cast">
+                            {member}
+                          </li>
+                        ))}
+                      </AccordionPanel>
+                    </AccordionItem>
+                  </Accordion>
+                  <Box mb={2}>{movieDetails.director}</Box>
+                  <Box>{movieDetails.productionCompanies.join(", ")}</Box>
+                </Box>
+              </Box>
+            </Box>
+          )}
+        </Box>
+      )}
+    </Box>
+  );
 }
-export default MovieDetails
+
+export default MovieDetails;
