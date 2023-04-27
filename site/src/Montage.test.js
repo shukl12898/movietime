e48cb02108a30b2af0bc2db9db0f9489853e46df
shukl12898 <shuklaak@usc.edu>
2023-04-26 20:56:03 -0700

    Tests fix

--- site/src/Montage.test.js
+++ site/src/Montage.test.js
@@ -1,49 +1,64 @@
 import React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import Montage from './pages/Montage';
 
-jest.mock('react-router-dom', () => ({
-  useNavigate: () => jest.fn(),
-  useLocation: () => ({
-    state: { movies: 550 },
-  }),
-}));
-
-describe('Montage Component', () => {
-  beforeEach(() => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve({ montageImages: [] }),
-      })
+describe('Montage', () => {
+  it('renders null when location state does not exist', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/montage" element={<Montage />} />
+        </Routes>
+      </MemoryRouter>
     );
-  });
-
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
-
-  it('renders the component', async () => {
-    const { getByTestId } = render(<Montage />);
-    expect(getByTestId('navbar')).toBeInTheDocument();
+    expect(screen.queryByTestId('picture-grid')).not.toBeInTheDocument();
   });
 
   it('fetches the images when movies is defined', async () => {
-    render(<Montage />);
+    const testMovies = {
+      movies: '550',
+    };
+
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve({ montageImages: [{ url: 'http://example.com/image1.jpg', title: 'Image 1' }] }),
+      })
+    );
+
+    render(
+      <MemoryRouter initialEntries={[{ pathname: '/montage', state: testMovies }]}>
+        <Routes>
+          <Route path="/montage" element={<Montage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
     await waitFor(() => {
       expect(global.fetch).toHaveBeenCalledWith('/images/550');
       expect(screen.getByTestId('picture-grid')).toBeInTheDocument();
     });
   });
 
- it('should log an error message if the fetch request fails', async () => {
-     jest.spyOn(console, 'error').mockImplementation(() => {});
-     jest.spyOn(global, 'fetch').mockRejectedValue(new Error('An error occurred'));
+  it('handles fetch errors', async () => {
+    const testMovies = {
+      movies: '550',
+    };
 
-     const { container } = render(<Montage />);
+    global.fetch = jest.fn(() => Promise.resolve({ status: 400 }));
 
-     await waitFor(() => {
-       expect(console.error).toHaveBeenCalledWith(new Error('An error occurred'));
-     });
-   });
+    render(
+      <MemoryRouter initialEntries={[{ pathname: '/montage', state: testMovies }]}>
+        <Routes>
+          <Route path="/montage" element={<Montage />} />
+        </Routes>
+      </MemoryRouter>
+    );
 
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalledWith('/images/550');
+      expect(screen.queryByTestId('picture-grid')).not.toBeInTheDocument();
+    });
+  });
 });
