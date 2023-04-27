3fc6839a0a0e3647ed37feec5e247783e819e2b7
shukl12898 <shuklaak@usc.edu>
2023-04-26 20:30:55 -0700

    Build fix

--- site/src/pages/Login.jsx
+++ site/src/pages/Login.jsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
 Flex, Spacer, Button
 } from '@chakra-ui/react';
-import NavBar from '../components/NavBar';
-
-import { useNavigate } from "react-router-dom";
 
 import LoginComponent from '../components/LoginComponent';
 import CreateAccount from '../components/CreateAccount';
@@ -13,7 +10,6 @@ function Login({toggleLogIn}) {
 
  // const [fetchResponse, handleFetchResponse] = useState();
   const [signUp, setSignUp] = useState(false);
-  const navigate = useNavigate();
 
      if (!signUp) {
         return (
